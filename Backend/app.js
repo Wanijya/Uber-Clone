c5c74a4343fa3f70072dba70595aadf09aaf8158
Wanijya <wanijyabisen@gmail.com>
2024-11-29 04:09:53 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,22 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", function(req, res) {
+  console.log("Hello, wor");
 })
+
+app.use("/users", userRoutes);
+
 
 module.exports = app;
