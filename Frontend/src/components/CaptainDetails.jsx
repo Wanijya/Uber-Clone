41cfd02ffbd6bbdb172b97a49d2d830b100dc158
Wanijya <wanijyabisen@gmail.com>
2025-01-22 02:59:51 +0530

    complete captain UI

---
+++ Frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const CaptainDetails = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src="https://imgs.search.brave.com/y-E9HTFDpnMgEq_6qhZ66_zapUkq93mqFGYpYQMJyUY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvY29v/bC1wcm9maWxlLXBp/Y3R1cmVzLW1vbmtl/eS1mYWNlLTBqeHdt/cTZicG0zaHM5Y2Iu/anBn"
+            alt=""
+          />
+          <h4 className="text-lg font-medium">Amit</h4>
+        </div>
+        <div>
+          <h4 className="text-xl font-semibold">₹193.20</h4>
+          <p className="text-sm text-gray-600">Earned</p>
+        </div>
+      </div>
+      <div className="flex p-3 mt-6 bg-gray-100 rounded-xl justify-center gap-5 items-start">
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-timer-2-line" />
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-speed-up-fill" />
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-booklet-line" />
+          <h5 className="text-lg font-medium">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainDetails;
